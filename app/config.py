96e8031ac969b77b1cd365c102c2086b409bea48
Roman Beskokotov <roma@gmail.com>
2025-09-25 15:19:40 +0300

    Добавил движок и вывод из  env файла

---
+++ app/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    
+    POSTGRES_HOST: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_PORT: int
+    POSTGRES_MODE: str
+    model_config = SettingsConfigDict(env_file='.env')
+
+    @property
+    def DATABASE_URL_async(self):
+        return f'postgres+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}'
+
+settings = Settings()
